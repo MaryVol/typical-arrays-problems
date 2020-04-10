e9cd2dfd17e724f9625302558b97dee5fd4c0b76
MaryVol <meryswhite@icloud.com>
2020-04-10 20:14:00 +0300

    array problems

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+var arr=[1,2,3,4]
+function max(){
+  let result= Math.max(...arr);
+  return result
+}
+max();
+
+function min(){
+  let result= Math.min(...arr);
+  return result
+}
+min();
+
+function avg(){
+  let result
+  arr.forEach(function(i){
+    result += parseInt(arr[i]);
+  })
+  result= parseInt(result)/arr.length
+  return result
+}
+avg();
 
 exports.min = function min (array) {
   return 0;
